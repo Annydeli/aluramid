4942d5089fd630ea41b753ec3e37e26b38f6bf9b
Annydeli <129961147+Annydeli@users.noreply.github.com>
2023-09-27 11:02:36 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-document.querySelector('.teclado');
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play('');
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+document.querySelector('.tecla_som').onclick = tocaSomPom();
+listaDeTeclas[1].onclick = tocaSomClap;
+listaDeTeclas[2].onclick = tocaSomTim;
+listaDeTeclas[3].onclick = tocaSomPuff;
